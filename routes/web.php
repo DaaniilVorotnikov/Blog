79b03dfd04e552cfe0ab75c01b338cf2dbccd391
DaaniilVorotnikov <daavorotnikov@gmail.com>
2021-10-21 18:21:16 +0300

    blog-commit-1

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\Blog\Admin\CategoryController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\Blog\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::prefix('blog')->group(function(){
+    Route::resource('posts', PostController::class)->names('blog.posts');
+});
+
+Route::resource('rest', RestTestController::class)->names('restTest');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin/blog')->group(function(){
+    //BlogCategory
+    $mehods = ['index', 'edit', 'store', 'update', 'create',];
+    Route::resource('categories', CategoryController::class)
+    ->only($mehods)
+    ->names('blog.admin.categories');
+});
